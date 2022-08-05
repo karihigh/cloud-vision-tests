1e5b03cac5e41f2601418cdc573b3a78926f5f96
Karina Hyland <khyland111@gmail.com>
2022-08-05 13:55:51 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// Imports the Google Cloud client library
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+/**
+ * TODO(developer): Uncomment the following line before running the sample.
+ */
+const fileName = '/img/69664655_463235944367110_5168726954769508226_n.jpg';
+
+// Read a local image as a text document
+const [result] = await client.documentTextDetection(fileName);
+const fullTextAnnotation = result.fullTextAnnotation;
+console.log(`Full text: ${fullTextAnnotation.text}`);
+fullTextAnnotation.pages.forEach(page => {
+  page.blocks.forEach(block => {
+    console.log(`Block confidence: ${block.confidence}`);
+    block.paragraphs.forEach(paragraph => {
+      console.log(`Paragraph confidence: ${paragraph.confidence}`);
+      paragraph.words.forEach(word => {
+        const wordText = word.symbols.map(s => s.text).join('');
+        console.log(`Word text: ${wordText}`);
+        console.log(`Word confidence: ${word.confidence}`);
+        word.symbols.forEach(symbol => {
+          console.log(`Symbol text: ${symbol.text}`);
+          console.log(`Symbol confidence: ${symbol.confidence}`);
+        });
+      });
+    });
+  });
+});
